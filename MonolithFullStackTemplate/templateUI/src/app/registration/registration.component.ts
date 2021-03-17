3723e8125d38e9ddad5c53a1ef507c8cbed62940
WilFerraciolli <wiliam334@hotmail.com>
2021-03-17 21:10:02 +0000

    Template UI - Added translations to login and register

--- MonolithFullStackTemplate/templateUI/src/app/registration/registration.component.ts
+++ MonolithFullStackTemplate/templateUI/src/app/registration/registration.component.ts
@@ -1,11 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {UserRegistration} from './user-registration';
-import {first} from 'rxjs/operators';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {AuthenticationService} from '../_services/authentication.service';
-import {NotificationService} from '../shared/notification.service';
-import {AuthService} from '../_services/auth-service';
+import { Component, OnInit } from '@angular/core';
+import { UserRegistration } from './user-registration';
+import { debounceTime, distinctUntilChanged, first, switchMapTo, take, tap } from 'rxjs/operators';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
+import { NotificationService } from '../shared/notification.service';
+import { AuthService } from '../_services/auth-service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -13,6 +16,8 @@ import {AuthService} from '../_services/auth-service';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+
+  private readonly _NAME_AVAILABILITY_URL = 'iam/users/usernames/availability?username=';
 
   registerForm: FormGroup;
   loading = false;
@@ -24,7 +29,8 @@ export class RegistrationComponent implements OnInit {
               private route: ActivatedRoute,
               private router: Router,
               private authenticationService: AuthService,
-              private notificationService: NotificationService) {
+              private notificationService: NotificationService,
+              private httpClient: HttpClient) {
 
     // redirect to home if already logged in
     // if (this.authenticationService.isUserLoggedOn) {
@@ -33,7 +39,7 @@ export class RegistrationComponent implements OnInit {
 
     this.authenticationService.isUserLoggedOn
       .subscribe(x => {
-        if (x === true){
+        if (x === true) {
           this.router.navigate(['/']);
         }
       });
@@ -85,4 +91,14 @@ export class RegistrationComponent implements OnInit {
         });
   }
 
+  public isUsernameAvailable(username: string): boolean {
+
+    this.httpClient.get<boolean>(environment.baseUrl + this._NAME_AVAILABILITY_URL + '?username=' + username)
+      .subscribe((data: boolean) => {
+
+        console.log('username availability', data);
+      });
+
+    return true;
+  }
 }
