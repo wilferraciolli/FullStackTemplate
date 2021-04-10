95e5391496da35743b559703c09010fdd5e1840d
WilFerraciolli <wiliam334@hotmail.com>
2021-04-10 08:24:42 +0100

    template API -  updated usernames end point

--- MonolithFullStackTemplate/templateAPI/src/main/java/com/template/users/UserRestService.java
+++ MonolithFullStackTemplate/templateAPI/src/main/java/com/template/users/UserRestService.java
@@ -12,6 +12,9 @@ import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.template.libraries.rest.BaseRestService;
+import com.template.libraries.rest.Metadata;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +22,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.template.libraries.rest.BaseRestService;
-import com.template.libraries.rest.Metadata;
 
 /**
  * The type User rest service.
@@ -124,8 +125,8 @@ public class UserRestService extends BaseRestService {
                 .build();
     }
 
-    @GetMapping("/usernames/{username}/availability")
-    public ResponseEntity<Boolean> checkUsernameAvailability(@PathVariable("username") final String username) {
+    @GetMapping("/usernames/availability")
+    public ResponseEntity<Boolean> checkUsernameAvailability(@RequestParam("username") final String username) {
 
         final boolean isAvailable = this.appService.checkUsernameAvailability(StringUtils.defaultString(username));
 
