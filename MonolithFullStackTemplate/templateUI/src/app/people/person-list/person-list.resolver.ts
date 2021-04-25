5dd96e61b607ee6c8ae7ef1c12a54bca5bef9c30
WilFerraciolli <wiliam334@hotmail.com>
2021-04-25 09:29:13 +0100

    Template API - Added person updated and deleted event

--- MonolithFullStackTemplate/templateUI/src/app/people/person-list/person-list.resolver.ts
+++ MonolithFullStackTemplate/templateUI/src/app/people/person-list/person-list.resolver.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { ProfileService } from '../../_services/profile.service';
-import { UserProfile } from '../../users/profile/user.profile';
-import { PeopleResponse } from './people-response';
-import { PersonService } from '../person.service';
-import { LoadingService } from '../../shared/components/loading/loading.service';
-import { finalize } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {ProfileService} from '../../_services/profile.service';
+import {UserProfile} from '../../users/profile/user.profile';
+import {PeopleResponse} from './people-response';
+import {PersonService} from '../person.service';
+import {LoadingService} from '../../shared/components/loading/loading.service';
+import {finalize} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {flatMap} from 'rxjs/internal/operators';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class PersonListResolver implements Resolve<Observable<PeopleResponse>> {
-
-  userProfile: UserProfile;
 
   constructor(private profileService: ProfileService,
               private personService: PersonService,
@@ -22,13 +21,10 @@ export class PersonListResolver implements Resolve<Observable<PeopleResponse>> {
 
     this.loadingService.loadingOn();
 
-    this.profileService.currentUserProfile
-      .subscribe(user => {
-        this.userProfile = user;
-      });
-
-    const url = this.userProfile.links.people.href;
-    return this.personService.getAll<PeopleResponse>(url)
-      .pipe(finalize(() => this.loadingService.loadingOff()));
+    return this.profileService.currentUserProfile
+      .pipe(flatMap((user: UserProfile) =>
+        this.personService.getAll<PeopleResponse>(user.links.people.href)
+          .pipe(finalize(() => this.loadingService.loadingOff()))
+      ));
   }
 }
