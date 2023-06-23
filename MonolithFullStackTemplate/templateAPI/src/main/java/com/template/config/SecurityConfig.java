52df03953581dee958d3cf1d06115dff9df7413b
WilFerraciolli <wiliam334@hotmail.com>
2023-06-24 00:03:50 +0300

    Template API - Fixing CORS

--- MonolithFullStackTemplate/templateAPI/src/main/java/com/template/config/SecurityConfig.java
+++ MonolithFullStackTemplate/templateAPI/src/main/java/com/template/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
+//                .requestMatchers("/**").permitAll()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
