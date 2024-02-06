6612595de8a8add4f253cd97741b9eee38705114
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-02-06 08:33:34 +0000

    Templateapi - Added CORS

--- MonolithFullStackTemplate/templateAPI/src/main/java/com/template/config/CorsConfig.java
+++ MonolithFullStackTemplate/templateAPI/src/main/java/com/template/config/CorsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 /**
  * The type Cors config.
  * Implement the CORS headers that will authorize access by clients from any domain
@@ -13,13 +15,15 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        String[] allowDomains = new String[2];
-        allowDomains[0] = "http://localhost:4200";
-        allowDomains[1] = "http://localhost:8080";
+        List<String> allowDomains = List.of(
+                "http://localhost:4200",
+                "http://localhost:8080",
+                "duckdns.org"
+        );
 
         registry
                 .addMapping("/**")
-                .allowedOrigins(allowDomains)
+                .allowedOrigins(allowDomains.toArray(new String[0]))
                 .allowedMethods("*");
     }
 }
